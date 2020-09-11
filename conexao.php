4724db958cdaa5ad199480bb052ebf8b21848ede
Mariana Cabrinha <70379460+Mariana-Cabrinha@users.noreply.github.com>
2020-09-10 22:01:56 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,3 +1,3 @@
-<?php $servidor = "127.0.0.1"; $usuario = "root"; $senha = ""; $banco = "";
+<?php $servidor = "127.0.0.1"; $usuario = "root"; $senha = ""; $banco = "pskids";
 $conexao = mysqli_connect ($servidor, $usuario, $senha) or die ("could not connect to mysql");
 mysqli_select_db ($conexao, $banco) or die ("could not select database"); ?>
